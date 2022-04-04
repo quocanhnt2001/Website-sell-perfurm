44cfbb48efa26f71acb568a1e89aa91ef2818d5b
quocanhnt2001 <quocanhnt2001@gmail.com>
2022-04-04 21:57:39 +0700

    Update

--- js/main.js
+++ js/main.js
@@ -50,17 +50,6 @@
         $(this).prev().removeClass('active');
     });
 
-    //Canvas Menu
-    $(".canvas__open").on('click', function () {
-        $(".offcanvas-menu-wrapper").addClass("active");
-        $(".offcanvas-menu-overlay").addClass("active");
-    });
-
-    $(".offcanvas-menu-overlay").on('click', function () {
-        $(".offcanvas-menu-wrapper").removeClass("active");
-        $(".offcanvas-menu-overlay").removeClass("active");
-    });
-
     /*-----------------------
         Hero Slider
     ------------------------*/
--- pages/shop.php
+++ pages/shop.php
@@ -77,14 +77,41 @@ include("../common/document_head.html");
                             </div>
                         </div>
                     </div>
-
                     <div class="row">
                         <div class="col-lg-4 col-md-6 col-sm-6">
-                            <?php
-                            include('../data/getnewproduct.php');
-                            $np = new ProductFeatured();
-                            $np -> ShowProductFeatured();
-                            ?> 
+                            <div class="product__item">
+                                <div class="product__item__pic set-bg" data-setbg="img/product/product-1.png">
+                                    <ul class="product__hover">
+                                        <li><a href="#"><img src="img/icon/heart.png" alt=""></a></li>
+                                        <li><a href="#"><img src="img/icon/compare.png" alt=""> <span>Compare</span></a>
+                                        </li>
+                                        <li><a href="#"><img src="img/icon/search.png" alt=""></a></li>
+                                    </ul>
+                                </div>
+                                <div class="product__item__text">
+                                    <h6>Tom Ford Ombré Leather</h6>
+                                    <a href="#" class="add-cart">+ Add To Cart</a>
+                                    <div class="rating">
+                                        <i class="fa fa-star-o"></i>
+                                        <i class="fa fa-star-o"></i>
+                                        <i class="fa fa-star-o"></i>
+                                        <i class="fa fa-star-o"></i>
+                                        <i class="fa fa-star-o"></i>
+                                    </div>
+                                    <h5>4,350,000đ</h5>
+                                    <div class="product__color__select">
+                                        <label for="pc-4">
+                                            <input type="radio" id="pc-4">
+                                        </label>
+                                        <label class="active black" for="pc-5">
+                                            <input type="radio" id="pc-5">
+                                        </label>
+                                        <label class="grey" for="pc-6">
+                                            <input type="radio" id="pc-6">
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div class="col-lg-4 col-md-6 col-sm-6">
                             <div class="product__item sale">
